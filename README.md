8740da11e52f8111e93c88c0cdf162110fb03b0f
Guapi test <katevladvlad@mail.ru>
2024-03-02 16:02:38 +0300

    Allure

--- README.md
+++
@@ -1,1 +0,0 @@
-[![Java CI with Gradle](https://github.com/Guap18/PATTERNS-1.0/actions/workflows/gradle.yml/badge.svg)](https://github.com/Guap18/PATTERNS-1.0/actions/workflows/gradle.yml)
--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,9 +1,10 @@
 package ru.netology;
 
 import com.codeborne.selenide.Condition;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
 
 import org.openqa.selenium.Keys;
 import ru.netology.data.DataGenerator;
@@ -16,6 +17,16 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CardDeliveryTest {
+
+    @BeforeAll
+    static void setAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll(){
+        SelenideLogger.removeListener("allure");
+    }
 
     @BeforeEach
     void setup() {
